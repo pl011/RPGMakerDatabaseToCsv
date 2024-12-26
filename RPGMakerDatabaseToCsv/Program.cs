5794da91393dd5e4b15fc3e4a1c13afc8063b193
pl011 <peter.954@outlook.com>
2024-12-26 16:28:26 +0000

    Get parsing of skills json working

--- RPGMakerDatabaseToCsv/Program.cs
+++ RPGMakerDatabaseToCsv/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using RPGMakerDatabaseToCsv;
+
 Console.WriteLine("Hello, World!");
+
+var result = JsonConvert.DeserializeObject<List<Skills>>(File.ReadAllText(args[0]));
+
+Console.WriteLine("Goodbye!");
